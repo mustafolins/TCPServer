df11a3d52683c108386c930ace797640161f8a67
Jonathan Van Dam <mustafolins@gmail.com>
2018-11-22 01:05:41 -0700

    Added Encryption
    Added encryption capabilities and some json formatting for the messages

--- server.go
+++ server.go
@@ -1,9 +1,22 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
+import (
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net" // only needed below for sample processing
+	"time"
+)
+
+type jMessage struct {
+	Msg string `json:"message"`
+}
 
 func main() {
 
@@ -18,12 +31,66 @@ func main() {
 	// run loop forever (or until ctrl-c)
 	for {
 		// will listen for message to process ending in newline (\n)
-		message, _ := bufio.NewReader(conn).ReadString('\n')
+		message, _ := bufio.NewReader(conn).ReadString('\n') // todo: change deliminater to something else
 		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
+		if len(message) > 0 {
+			byt := decrypt([]byte(message[:len(message)-1]), "password")
+			var dat map[string]interface{}
+			json.Unmarshal(byt, &dat)
+
+			fmt.Println("Message Received:", dat["message"].(string))
+
+			outgoingMessage := &jMessage{
+				Msg: dat["message"].(string), // remove \n from message
+			}
+			outBytes, err := json.Marshal(outgoingMessage)
+			if err != nil {
+				continue
+			}
+			sendText := string(encrypt([]byte(outBytes), "password"))
+			// send new string back to client
+			conn.Write([]byte(sendText + "\n"))
+		} else {
+			time.Sleep(time.Millisecond * 5)
+		}
 	}
+}
+
+func decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
